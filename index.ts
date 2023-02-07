d54efe23626382b2401005ddaa7ca8d7663074d0
Jakub Zakrzewski <jz.zakrzewsky@gmail.com>
2023-02-07 16:35:53 +0100

    morgan, cleanup

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { config } from "dotenv";
+import morgan from "morgan";
+import { spawn } from "child_process";
 
 config();
 
 const app = express();
+app.use(morgan("tiny"));
 
 const port = process.env.PORT || 3030;
 
@@ -12,7 +15,6 @@ app.listen(port, function () {
 });
 
 app.get("/", function (req, res) {
-  const spawn = require("child_process").spawn;
   const process = spawn("python", ["./df/get_data.py"]);
   process.stdout.on("data", function (data: string) {
     res.send(data.toString());
